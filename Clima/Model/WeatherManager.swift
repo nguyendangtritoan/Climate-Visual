9c3fe527c649c43188d1b16fd71e101eb8b2ab3a
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2021-03-01 14:12:25 +0200

    Codable JSON

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -14,7 +14,7 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        //print(urlString)
         performRequest(urlString: urlString)
     }
     
@@ -30,7 +30,7 @@ struct WeatherManager {
                 }
                 
                 if let safeData = data {
-                    parseJSON(weatherData: safeData)
+                    self.parseJSON(weatherData: safeData)
                 }
             }
             task.resume()
@@ -41,10 +41,18 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodeData.main.temp)
-            print(decodeData.weather[0].description)
+            let id = decodeData.weather[0].id
+            let temp = decodeData.main.temp
+            let name = decodeData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName,weather.temperature)
+            
         }catch{
             print(error)
         }
-        }
+    }
+    
+    
 }
